2b05d371fda81c96b78379936063a602d917cf35
Dhanyathaa M <kit27.am14@gmail.com>
2025-12-19 22:35:49 +0530

    Add DFS problem statement
    Add comments explaining the Depth First Search algorithm.

---
+++ Graphs/DFS.java
@@ -0,0 +1,3 @@
+// Depth First Search (DFS)
+// Given a graph, perform Depth First Search traversal starting from a given node.
+// DFS explores as far as possible along each branch before backtracking.
